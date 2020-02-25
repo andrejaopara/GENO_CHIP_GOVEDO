48f0318dab15f5b76b4b2922fc6f733fd8d207db
janaobsteter <obsteter.jana@gmail.com>
2020-02-25 09:01:24 +0100

    Erase

--- Crnobela_IDSeq.sql
+++ Crnobela_IDSeq.sql
@@ -4,5 +4,26 @@ SELECT ziv.DRZ_ORIG_ZIVAL
   ziv.DAT_ROJSTVO,
   ziv.SIF_SPOL
 FROM zivali ziv
-WHERE ziv.SP1_SIFRA_PASMA in (3,15,9);
+;
+
+
+
+SELECT ziv.DRZ_ORIG_ZIVAL
+  || ziv.STEV_ORIG_ZIVAL "ID_ZIVALI",
+  ziv.ZIV_ID_SEQ,
+  ziv.DAT_ROJSTVO,
+  ziv.SIF_SPOL
+FROM zivali ziv
+WHERE extract ( year from ziv.DAT_ROJSTVO) > 2000;
+
+select * from zivali ziv where  ziv.STEV_ORIG_ZIVAL = '54565740' ;
+
+
+SELECT ziv.DRZ_ORIG_ZIVAL
+  || ziv.STEV_ORIG_ZIVAL "ID_ZIVALI",
+  ziv.ZIV_ID_SEQ,
+  ziv.DAT_ROJSTVO,
+  ziv.SIF_SPOL
+FROM zivali ziv
+WHERE  ziv.SP1_SIFRA_PASMA=15;
 
