d2c35e43d55f317920773ee7468c9b0dce6f683e
andrejaopara <andreja.opara@gmail.com>
2020-04-07 21:13:46 +0200

    Popravki po sortiranju FinalReporta

--- OrderPedFile.R
+++ OrderPedFile.R
@@ -2,7 +2,7 @@ args = commandArgs(trailingOnly=TRUE)
 pedfile = args[1]
 
 print(pedfile)
-fr <- read.table(pedfile, sep="\t", skip=9, header=TRUE)
-fr <- fr[order(fr$Sample.ID),]
+fr <- read.table(pedfile, sep="\t", skip=10, header=FALSE)
+fr <- fr[order(fr$V2),]
 
-write.table(fr, "OrderedPedFile.txt", sep="\t", quote=FALSE, row.names=FALSE)
+write.table(fr, "OrderedPedFile.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names = FALSE)
--- SortFinalReport.sh
+++ SortFinalReport.sh
@@ -2,7 +2,7 @@ pedfile=$1
 codedir=$2
 echo $pedfile
 echo $codedir
-head -n9 $pedfile  > Header.txt
+head -n10 $pedfile  > Header.txt
 Rscript ${codedir}/OrderPedFile.R $pedfile
 cat Header.txt OrderedPedFile.txt > PEDFILE.txt
 mv $pedfile ${pedfile}_unsorted
