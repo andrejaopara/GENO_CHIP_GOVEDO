10298736578d346c9adc6cd5dccb143d34dcd506
janaobsteter <obsteter.jana@gmail.com>
2021-11-29 11:09:18 +0100

    Edited QC file, corrected mistakes and added messages

--- 1_QC_FileArgs.sh
+++ 1_QC_FileArgs.sh
@@ -9,33 +9,54 @@ export S=$4
 echo "##########################################################################"
 echo "Check individual call rate"
 echo "##########################################################################"
-## Check for genotype call, heterozygosity
+
+echo "##########################################################################"
+echo "Check for genotype call, heterozygosity"
+echo "##########################################################################"
 $3 --file $1 --missing --het --cow --recode --out $1_$2
 
-## Extrac the individuals that have genotype call less than 0.9
+
+echo "##########################################################################"
+echo "Extrac the individuals that have genotype call less than 0.9"
+echo "##########################################################################"
 tail -n +2 $1_$2.imiss | awk '$6 >= 0.10 { print $1,$2 }' > IDsWithCallRateLessThan0.90$1_$2.txt
 
+
+echo "##########################################################################"
+echo "Draw plot for percentage of genotype call rate and heterozygosity rate for individuals"
+echo "##########################################################################"
 ## Draw plot for percentage of genotype call rate and heterozygosity rate for individuals
 ## You need geneplotter package to run next script. Install geneplotter with the following command
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("geneplotter")
 ${S}/DrawHetMissPlotInd.R $1_$2
+echo "##########################################################################"
 echo "Created plot:$1_$2_imiss-vs-het.pdf"
+echo "##########################################################################"
 
 
 ## Remove individuals with low genotype call rate and heterozygosity rate out of range +-6SD of heterozygosity
 FILE=./IndividualsToExlcudeMissHet.txt
 if [ -f "$FILE" ]; then
+      echo "##########################################################################"
+      echo "File IndividualsToExclude.txt exists"
+      echo "##########################################################################"
       $3 --file $1_$2 --remove IndividualsToExlcudeMissHet.txt --missing --recode --cow --out $1_$2_CleanInds
       mv IndividualsToExlcudeMissHet.txt IndividualsToExlcudeMissHet_$1_$2.txt
 else 
+      echo "##########################################################################"
+      echo "File IndividualsToExclude.txt DOESNT exists"
+      echo "##########################################################################"
       mv $1_$2.ped $1_$2_CleanInds.ped
       mv $1_$2.map $1_$2_CleanInds.map
       mv $1_$2.lmiss  $1_$2_CleanInds.lmiss
 fi
 
-FILE2=$1_$2_CleanInds
+FILE2=$1_$2_CleanInds.ped
 if [ -f $FILE2 ]; then 
+        echo "##########################################################################"
+        echo "File CleanIndsMarkers.txt exists"
+        echo "##########################################################################"
 	echo "##########################################################################"
 	echo "Check for SNP call rate"
 	echo "##########################################################################"
