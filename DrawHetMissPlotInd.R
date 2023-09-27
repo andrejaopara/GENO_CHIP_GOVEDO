b3622643b104b20a737d63a9a892d64ef2c89dee
janaobsteter <obsteter.jana@gmail.com>
2023-09-27 08:52:47 +0200

    Changing Draw scripts to use ggplot2

--- DrawHetMissPlotInd.R
+++ DrawHetMissPlotInd.R
@@ -15,16 +15,18 @@ imiss$logF_MISS = log10(imiss$F_MISS)
 het$meanHet = (het$N.NM. - het$O.HOM.)/het$N.NM.
 df <- merge(het, imiss, by = "IID")
 
-pdf(paste0(fileName, "_imiss-vs-het.pdf"), width=13/2.54, height=13/2.54, pointsize=12)
-ggplot(data = df, aes(x = logF_MISS, y = meanHet)) + geom_point() +
-  geom_hline(aes(yintercept = mean(het$meanHet, na.rm=TRUE)-(6*sd(het$meanHet, na.rm=TRUE))), linetype=2) +
-  geom_hline(aes(yintercept = mean(het$meanHet, na.rm=TRUE)+(6*sd(het$meanHet, na.rm=TRUE))), linetype=2) +
-  geom_vline(aes(xintercept = log10(0.10)), linetype=2) +
-  scale_x_continuous(breaks = c(seq(-5, 0, 1)), labels = c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1), limits = c(-5, 0)) +
-  ylim(c(0, 1)) +
-  xlab("Proportion of missing genotypes") +
-  ylab("Heterozygosity rate")
-dev.off()
+if (nrow(imiss) > 0) {
+  pdf(paste0(fileName, "_imiss-vs-het.pdf"), width=13/2.54, height=13/2.54, pointsize=12)
+  ggplot(data = df, aes(x = logF_MISS, y = meanHet)) + geom_point() +
+    geom_hline(aes(yintercept = mean(het$meanHet, na.rm=TRUE)-(6*sd(het$meanHet, na.rm=TRUE))), linetype=2) +
+    geom_hline(aes(yintercept = mean(het$meanHet, na.rm=TRUE)+(6*sd(het$meanHet, na.rm=TRUE))), linetype=2) +
+    geom_vline(aes(xintercept = log10(0.10)), linetype=2) +
+    scale_x_continuous(breaks = c(seq(-5, 0, 1)), labels = c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1), limits = c(-5, 0)) +
+    ylim(c(0, 1)) +
+    xlab("Proportion of missing genotypes") +
+    ylab("Heterozygosity rate")
+  dev.off()
+}
 
 
 MISS <- data.frame(imiss[imiss$F_MISS > 0.10, c("FID","IID")])
